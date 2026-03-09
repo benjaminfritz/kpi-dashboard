0c008b55395e6c5d692f9c85361ad6e43356153f
Benjamin Fritz <hibenjaminfritz@gmail.com>
2026-03-09 23:35:34 +0100

    Display five top teams

--- api/figma-analytics.js
+++ api/figma-analytics.js
@@ -1,7 +1,7 @@
 const FIGMA_API_BASE_URL = "https://api.figma.com/v1";
 const LOOKBACK_DAYS_DEFAULT = 30;
 const MAX_TOP_COMPONENTS = 5;
-const MAX_TOP_TEAMS = 3;
+const MAX_TOP_TEAMS = 5;
 
 const sumBy = (rows, key) =>
   rows.reduce((sum, row) => {
